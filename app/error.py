7b5ff6f507ffb57a62a2274d7df7b3357d356243
enockabere <maebaenock95@gmail>
2021-08-16 11:59:11 +0300

    functin to render 404 error page

---
+++ app/error.py
@@ -0,0 +1,9 @@
+from  flask import  render_template
+from app import app
+
+@app.errorhandler(404)
+def four_ow_four(error):
+    '''
+    function to render 404 error page
+    '''
+    return render_template("error.html"),404
