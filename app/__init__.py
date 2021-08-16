4588edb1c2d4e2ec99deae79d66cc855dfe447c4
enockabere <maebaenock95@gmail>
2021-08-16 13:10:27 +0300

    create user login form

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,11 @@
 from flask import  Flask
+from flask_bootstrap import Bootstrap
 
 #initializing flask application
 app = Flask(__name__)
+app.config['SECRET_KEY']= 'Thisissupposedtobesecret'
 
+# Initializing Flask Extensions
+bootstrap = Bootstrap(app)
+                      
 from app import  views,error
--- app/views.py
+++ app/views.py
@@ -1,5 +1,13 @@
 from flask import render_template
 from app import  app
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,BooleanField
+from wtforms.validators import InputRequired,Email,Length
+
+class LoginForm(FlaskForm):
+   username = StringField('username', validators=[InputRequired(),Length(min=4,max=20)])
+   password = PasswordField('password', validators=[InputRequired(),Length(min=6,max=20)])
+   remember = BooleanField('remember me')
 
 #views
 @app.route('/')
@@ -8,3 +16,12 @@ def index():
     view root page function that returns indext.html and its data
     '''
     return render_template('index.html')
+@app.route('/login')
+def login():
+    '''Instantiate login form'''
+    form = LoginForm()
+    
+    return render_template('login.html',form=form)
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
