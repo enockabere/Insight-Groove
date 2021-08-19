747f48479cde81cea4e20481f8c30d5e5d85e07b
enockabere <maebaenock95@gmail>
2021-08-19 22:29:41 +0300

    create user dashboard template

--- app/main/forms.py
+++ app/main/forms.py
@@ -14,4 +14,4 @@ class  RegisterForm(FlaskForm):
     password = PasswordField('password', validators=[InputRequired(),Length(min=6,max=80)])
 class PitchForm(FlaskForm):
    message = StringField('message', widget=TextArea(), validators=[InputRequired()])
-   category = SelectField('Pitch Category',choices=[('love','love'),('positive','positive'),('meme','meme'),('family','family')])
+   category = SelectField('Pitch Category',choices=[('Love','Love'),('Positive','Positive/Inspiring'),('Meme','Memes/Funny'),('Family','Family/Life')])
