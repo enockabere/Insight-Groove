e03b6761d005806683cd4147cb6fdedac9e06d27
enockabere <maebaenock95@gmail>
2021-08-21 00:24:21 +0300

    create a database table model forstoring post votes

--- app/models.py
+++ app/models.py
@@ -23,5 +23,11 @@ class Pitch (db.Model):
     category = db.Column(db.String(20),nullable=False)
     created_at = db.Column(db.DateTime(), default=datetime.utcnow(), nullable=False)
     user_id = db.Column(db.Integer,db.ForeignKey('users.id') )
+class Post(db.Model):
+    __tablename__= 'posts'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer.ForeignKey('users.id'))
+    pitch_id = db.Column(db.Integer.ForeignKey('pitchs.id'))
     
     
