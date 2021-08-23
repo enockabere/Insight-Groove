ef6392241ab2465743d56952dc8cc5d03f67bbe9
enockabere <maebaenock95@gmail>
2021-08-23 10:03:17 +0300

    deployment

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     '''
     SECRET_KEY= os.environ.get('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://enock:Maeba1995@localhost/pitch'
 
 class ProdConfig(Config):
     
