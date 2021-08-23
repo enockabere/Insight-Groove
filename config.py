bf6f28a23d1e74bd2f19cd72b8ad687306d3eeb6
enockabere <maebaenock95@gmail>
2021-08-23 09:56:08 +0300

    add deployment files

--- config.py
+++ config.py
@@ -1,16 +1,14 @@
 import  os
-import secrets
 class Config:
     '''
     General configuration parent class
     '''
     SECRET_KEY= os.environ.get('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://enock:Maeba1995@localhost/pitch'
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
 
 class ProdConfig(Config):
     
-    SQLALCHEMY_DATABASE_URI= ''
     DEBUG = True
 
 class DevConfig(Config):
