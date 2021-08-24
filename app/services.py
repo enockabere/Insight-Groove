b3cc6df2ad8d6cc0ebcd1c6d78699a3702c555d5
enockabere <maebaenock95@gmail>
2021-08-24 14:36:37 +0300

    add a class to define quote objects

---
+++ app/services.py
@@ -0,0 +1,7 @@
+class Quotes:
+    '''
+    class defines quotes objects
+    '''
+    def __init__(self, author, quote):
+        self.author = author
+        self.quote = quote
--- config.py
+++ config.py
@@ -6,6 +6,7 @@ class Config:
     SECRET_KEY= os.environ.get('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://enock:Maeba1995@localhost/blog'
+    QUOTES_API_BASE_URL = 'https://newsapi.org/v2/everything?q={}&sortBy=popularity&apiKey={}'
 
 class ProdConfig(Config):
 
